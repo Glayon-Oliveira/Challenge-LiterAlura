779df3d82a36861673f098d3e12b6692f9dcf2bf
Solansra <solansra@gmail.com>
2024-11-06 17:57:09 -0300

    Set properties and methods of the AuthorDTO class

--- src/main/java/com/lmlasmo/literalura/dto/AuthorDTO.java
+++ src/main/java/com/lmlasmo/literalura/dto/AuthorDTO.java
@@ -1,5 +1,40 @@
 package com.lmlasmo.literalura.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class AuthorDTO {
+
+	@JsonProperty("name")
+	private String name;
+
+	@JsonProperty("birth_year")
+	private int birthYear;
+
+	@JsonProperty("death_year")
+	private int deathYear;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+
+	public int getDeathYear() {
+		return deathYear;
+	}
+
+	public void setDeathYear(int deathYear) {
+		this.deathYear = deathYear;
+	}
 
 }
