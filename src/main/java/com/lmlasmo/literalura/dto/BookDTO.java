3ac682cdc15d13e6dd71572b274b3e3207fc4bca
Solansra <solansra@gmail.com>
2024-11-06 18:05:13 -0300

    Set properties and methods of the BookDTO class

--- src/main/java/com/lmlasmo/literalura/dto/BookDTO.java
+++ src/main/java/com/lmlasmo/literalura/dto/BookDTO.java
@@ -1,5 +1,55 @@
 package com.lmlasmo.literalura.dto;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BookDTO {
+	
+	@JsonProperty("title")
+	private String title;
+	
+	@JsonProperty("authors")
+	private List<AuthorDTO> authors;
+	
+	@JsonProperty("languages")
+	private List<String> languages;
+	
+	@JsonProperty("download_count")
+	private int downloadCount;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<AuthorDTO> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<AuthorDTO> authors) {
+		this.authors = authors;
+	}
+
+	public List<String> getLanguages() {
+		return languages;
+	}
+
+	public void setLanguages(List<String> languages) {
+		this.languages = languages;
+	}
+
+	public int getDownloadCount() {
+		return downloadCount;
+	}
+
+	public void setDownloadCount(int downloadCount) {
+		this.downloadCount = downloadCount;
+	}	
 
 }
