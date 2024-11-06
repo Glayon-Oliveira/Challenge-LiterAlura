97b08d8fed634a976297dac2a055582a5a094911
Solansra <solansra@gmail.com>
2024-11-06 20:54:47 -0300

    Set the requestAuthorByName method and complementary methods in the ConsumeGuntendexAPI class

--- src/main/java/com/lmlasmo/literalura/service/ConsumeGuntendexAPI.java
+++ src/main/java/com/lmlasmo/literalura/service/ConsumeGuntendexAPI.java
@@ -104,5 +104,48 @@ public class ConsumeGuntendexAPI {
 		}		
 	
 	}
+	
+	public AuthorDTO requestAuthorByName(String name){
+		
+		List<BookDTO> bookList = requestSearch(name);
+		
+		List<AuthorDTO> authorList = bookList.stream()
+					.flatMap(b -> b.getAuthors().stream())
+					.filter(a -> verifyAuthorName(a.getName(), name))
+					.distinct()
+					.collect(Collectors.toList);
+					
+		if(authorList.size()>0){
+			return authorList.get(0);
+		}else{
+			return null;
+		}
+		
+	}
+	
+	private boolean verifyAuthorName(String authorName, String name){
+	
+	
+		for(int cc = 0; cc<name.length()-1;){
+						
+			int nextSpaceIndex = name.indexOf(" ", cc);
+							
+			if (nextSpaceIndex == -1) {
+			       nextSpaceIndex = name.length();
+		        }
+							
+			String subname = name.substring(cc, nextSpaceIndex);
+							
+			if (authorName.contains(subname)) {
+			        return false;
+			}
+
+			cc = nextSpaceIndex;
+													
+		}
+						
+		return true;					
+	
+	}
 
 }
