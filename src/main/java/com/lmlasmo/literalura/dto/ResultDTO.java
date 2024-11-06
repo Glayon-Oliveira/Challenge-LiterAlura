f0653e8ca357b9462688fd07b03aea69d373513e
Solansra <solansra@gmail.com>
2024-11-06 18:37:45 -0300

    Set properties and methods of the ResultDTO class

--- src/main/java/com/lmlasmo/literalura/dto/ResultDTO.java
+++ src/main/java/com/lmlasmo/literalura/dto/ResultDTO.java
@@ -1,5 +1,55 @@
 package com.lmlasmo.literalura.dto;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ResultDTO {
+	
+	@JsonProperty("count")
+	private int count;
+	
+	@JsonProperty("next")
+	private String next;
+	
+	@JsonProperty("previous")
+	private String previous;
+	
+	@JsonProperty("results")
+	private List<BookDTO> results;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public String getNext() {
+		return next;
+	}
+
+	public void setNext(String next) {
+		this.next = next;
+	}
+
+	public String getPrevious() {
+		return previous;
+	}
+
+	public void setPrevious(String previous) {
+		this.previous = previous;
+	}
+
+	public List<BookDTO> getResults() {
+		return results;
+	}
+
+	public void setResults(List<BookDTO> results) {
+		this.results = results;
+	}	
 
 }
