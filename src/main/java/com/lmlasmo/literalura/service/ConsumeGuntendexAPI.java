20e785a51548519d13af7b36d677d4fcf429eba5
Solansra <solansra@gmail.com>
2024-11-06 17:05:25 -0300

    Set properties of the ConsumeGuntendexAPI class

--- src/main/java/com/lmlasmo/literalura/service/ConsumeGuntendexAPI.java
+++ src/main/java/com/lmlasmo/literalura/service/ConsumeGuntendexAPI.java
@@ -1,5 +1,14 @@
 package com.lmlasmo.literalura.service;
 
+import java.net.http.HttpClient;
+
 public class ConsumeGuntendexAPI {
+	
+	private final String API_HOST = "https://gutendex.com/";
+	private HttpClient client;
+	
+	public ConsumeGuntendexAPI() {
+		
+	}	
 
 }
