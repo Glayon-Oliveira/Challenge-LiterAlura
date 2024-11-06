3f25c3a4a9e01e8215fd7b09e0189841eaf85819
Solansra <solansra@gmail.com>
2024-11-06 18:44:44 -0300

    Set the requestSearch method in the ConsumeGuntendexAPI class

--- src/main/java/com/lmlasmo/literalura/service/ConsumeGuntendexAPI.java
+++ src/main/java/com/lmlasmo/literalura/service/ConsumeGuntendexAPI.java
@@ -1,6 +1,16 @@
 package com.lmlasmo.literalura.service;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.lmlasmo.literalura.dto.BookDTO;
+import com.lmlasmo.literalura.dto.ResultDTO;
 
 public class ConsumeGuntendexAPI {
 	
@@ -12,5 +22,71 @@ public class ConsumeGuntendexAPI {
 		client = HttpClient.newHttpClient();
 		
 	}	
+	
+	public List<BookDTO> requestBooks(String title) {
+		
+		List<BookDTO> bookList = new ArrayList<BookDTO>();
+		
+		try {
+			
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(new URI(String.join(this.API_HOST, "/books?search=", title)))
+					.GET()
+					.build();
+			
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			
+			if(response.statusCode() == 200) {
+				
+				String jsonResponse = response.body();
+				
+				JsonMapper mapper = new JsonMapper();
+				
+				ResultDTO result = mapper.readValue(jsonResponse, ResultDTO.class);
+				
+				if(result.getCount() > 0) {
+					
+					bookList.addAll(result.getResults());
+					
+					Optional<String> next = Optional.ofNullable(result.getNext());
+					Optional<String> previous = Optional.ofNullable(result.getPrevious());										
+					
+					while(next.isPresent()) {
+						
+						request = HttpRequest.newBuilder()
+								.uri(new URI(next.get()))
+								.GET()
+								.build();
+						
+						response = client.send(request, HttpResponse.BodyHandlers.ofString());
+						
+						if(response.statusCode() == 200) {
+							
+							String jsonNextResposta = response.body();
+							
+							ResultDTO nextResult = mapper.readValue(jsonNextResposta, ResultDTO.class);
+							
+							bookList.addAll(nextResult.getResults());
+							
+							next = Optional.ofNullable(nextResult.getNext());
+							
+						}else break;										
+						
+					}
+					
+				}								
+				
+			}
+			
+		}catch(Exception e) {
+			System.out.println("Erro de requisição");
+			e.printStackTrace();		
+		}			
+		
+		return bookList;
+		
+	}
+	
+	public BookDTO 
 
 }
