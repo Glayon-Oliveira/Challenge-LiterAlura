50d742c328dc2247d7126858739bdcc687b39ea0
Solansra <solansra@gmail.com>
2024-11-08 13:58:56 -0300

    Defining properties

--- src/main/java/com/lmlasmo/literalura/service/Menu.java
+++ src/main/java/com/lmlasmo/literalura/service/Menu.java
@@ -1,5 +1,31 @@
 package com.lmlasmo.literalura.service;
 
-public class Menu {
+import java.util.Scanner;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lmlasmo.literalura.repository.AuthorRepository;
+import com.lmlasmo.literalura.repository.BookRepository;
+
+@Service
+public class Menu {
+	
+	
+	private ConsumeGuntendexAPI api;
+	private Scanner scanner;
+	
+	private BookRepository bookRepository;
+	private AuthorRepository authorRepository;	
+	
+	@Autowired
+	public Menu(BookRepository bookRepository, AuthorRepository authorRepository) {
+		
+		this.bookRepository = bookRepository;
+		this.authorRepository = authorRepository;
+		this.api = new ConsumeGuntendexAPI();
+		this.scanner = new Scanner(System.in);
+		
+	}
+	
 }
