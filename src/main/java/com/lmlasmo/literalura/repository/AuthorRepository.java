76c533e113d15dc4c959dd83dbafeed1c9913c5b
Solansra <solansra@gmail.com>
2024-11-08 18:57:57 -0300

    Change in return types of methods

--- src/main/java/com/lmlasmo/literalura/repository/AuthorRepository.java
+++ src/main/java/com/lmlasmo/literalura/repository/AuthorRepository.java
@@ -1,6 +1,7 @@
 package com.lmlasmo.literalura.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,7 +11,7 @@ import com.lmlasmo.literalura.model.Author;
 
 public interface AuthorRepository extends JpaRepository<Author, Long>{
 	
-	public Author findByName(String name);
+	public Optional<Author> findByName(String name);
 	
 	@Query("SELECT a FROM Author a WHERE YEAR(a.birthYear) <= :ano AND (a.deathYear IS NULL || OR YEAR(a.deathYear) >= :ano)")
 	public List<Author> findLivingAuthors(@Param("ano") int ano);
