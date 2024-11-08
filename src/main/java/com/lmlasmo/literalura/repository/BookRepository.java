74ef1c61a68745d427d04d329dbc40f88d3ad595
Solansra <solansra@gmail.com>
2024-11-08 01:20:22 -0300

    Defining derived methods

--- src/main/java/com/lmlasmo/literalura/repository/BookRepository.java
+++ src/main/java/com/lmlasmo/literalura/repository/BookRepository.java
@@ -1,9 +1,15 @@
 package com.lmlasmo.literalura.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lmlasmo.literalura.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long>{
+	
+	public Book findByTitle(String title);
+	
+	public List<Book> findByAuthorName(String name);
 
 }
