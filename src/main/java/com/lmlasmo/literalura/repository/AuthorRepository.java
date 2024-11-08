2e86e74f1afeec18d6432168309ff2bd52e1503e
Solansra <solansra@gmail.com>
2024-11-08 01:48:26 -0300

    Defining methods

--- src/main/java/com/lmlasmo/literalura/repository/AuthorRepository.java
+++ src/main/java/com/lmlasmo/literalura/repository/AuthorRepository.java
@@ -1,7 +1,18 @@
 package com.lmlasmo.literalura.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface AuthorRepository extends JpaRepository{
+import com.lmlasmo.literalura.model.Author;
 
+public interface AuthorRepository extends JpaRepository<Author, Long>{
+	
+	public Author findByName(String name);
+	
+	@Query("SELECT a FROM Author a WHERE YEAR(a.birthYear) <= :ano AND (a.deathYear IS NULL || OR YEAR(a.deathYear) >= :ano)")
+	public List<Author> findLivingAuthors(@Param("ano") int ano);
+	
 }
