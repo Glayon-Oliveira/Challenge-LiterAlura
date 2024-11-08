d1a3ade03ca73246127a9bcd88c1084f993e26b9
Solansra <solansra@gmail.com>
2024-11-08 00:47:06 -0300

    Defined the relationship of entities

--- src/main/java/com/lmlasmo/literalura/model/Authors.java
+++ src/main/java/com/lmlasmo/literalura/model/Authors.java
@@ -1,10 +1,16 @@
 package com.lmlasmo.literalura.model;
 
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +29,12 @@ public class Authors {
 	
 	@Column(name = "death_year")
 	private int deathYear;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "book_author",
+			   joinColumns = @JoinColumn(name = "author_id"),
+			   inverseJoinColumns = @JoinColumn(name = "book_id"))
+	private Set<Book> books;
 	
 	public Authors() {}
 	
--- src/main/java/com/lmlasmo/literalura/model/Book.java
+++ src/main/java/com/lmlasmo/literalura/model/Book.java
@@ -1,10 +1,14 @@
 package com.lmlasmo.literalura.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +27,9 @@ public class Book {
 	
 	@Column(name = "download_count")
 	private long downloadCount;
+	
+	@ManyToMany(mappedBy = "books")	
+	private Set<Authors> authors = new HashSet<Authors>();
 	
 	public Book() {}
 
