5d042ed83863fa9497af389526775a1b100ea18e
Solansra <solansra@gmail.com>
2024-11-08 00:52:03 -0300

    Getters and Setters

--- src/main/java/com/lmlasmo/literalura/model/Author.java
+++ src/main/java/com/lmlasmo/literalura/model/Author.java
@@ -37,5 +37,45 @@ public class Author {
 	private Set<Book> books;
 	
 	public Author() {}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+
+	public int getDeathYear() {
+		return deathYear;
+	}
+
+	public void setDeathYear(int deathYear) {
+		this.deathYear = deathYear;
+	}
+
+	public Set<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}	
 	
 }
--- src/main/java/com/lmlasmo/literalura/model/Book.java
+++ src/main/java/com/lmlasmo/literalura/model/Book.java
@@ -33,4 +33,44 @@ public class Book {
 	
 	public Book() {}
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getLanguages() {
+		return languages;
+	}
+
+	public void setLanguages(String languages) {
+		this.languages = languages;
+	}
+
+	public long getDownloadCount() {
+		return downloadCount;
+	}
+
+	public void setDownloadCount(long downloadCount) {
+		this.downloadCount = downloadCount;
+	}
+
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}	
+
 }
