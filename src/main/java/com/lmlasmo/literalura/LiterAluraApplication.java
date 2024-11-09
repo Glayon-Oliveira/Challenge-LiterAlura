4fcf5d2ef114e4f138158a2b04227495316f0b28
Solansra <solansra@gmail.com>
2024-11-08 23:15:09 -0300

    Menu access set

--- src/main/java/com/lmlasmo/literalura/LiterAluraApplication.java
+++ src/main/java/com/lmlasmo/literalura/LiterAluraApplication.java
@@ -1,11 +1,21 @@
 package com.lmlasmo.literalura;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lmlasmo.literalura.service.Menu;
+
 @SpringBootApplication
 public class LiterAluraApplication implements CommandLineRunner{
+	
+	private Menu menu;
+	
+	@Autowired
+	public LiterAluraApplication(Menu menu) {
+		this.menu = menu;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
@@ -14,7 +24,7 @@ public class LiterAluraApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		
+		menu.showMenu();
 		
 	}
 
