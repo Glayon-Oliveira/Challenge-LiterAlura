61ab37aed1a0402a4f25c32797321447a5a29db2
Solansra <solansra@gmail.com>
2024-11-08 23:11:18 -0300

    Changing Return Type

--- src/main/java/com/lmlasmo/literalura/repository/LanguageRepository.java
+++ src/main/java/com/lmlasmo/literalura/repository/LanguageRepository.java
@@ -1,11 +1,13 @@
 package com.lmlasmo.literalura.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lmlasmo.literalura.model.Language;
 
 public interface LanguageRepository extends JpaRepository<Language, Long>{
 	
-	public Language findByLanguage(String language);
+	public Optional<Language> findByLanguage(String language);	
 
 }
